9675462a06c381fb6543e1cad5ca8cee6ced67b6
Yanina Lubenska <yaninaosadchaya@gmail.com>
2022-07-19 12:02:49 +0300

    Added a correct URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name="Clean Folder",
       version="1.0",
       description="Folder organizer",
-      url="https://github.com/YaninaLu/folders-organizer/tree/main/clean_folder",
+      url="https://github.com/YaninaLu/folders-organizer",
       author="Yanina Lubenska",
       author_email="yaninaosadchaya@gmail.com",
       packages=["clean"]
