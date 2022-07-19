5a4cd386f7cb2275489214e2555e67ecef9b492e
Yanina Lubenska <yaninaosadchaya@gmail.com>
2022-07-19 11:58:18 +0300

    Fix folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="Clean Folder",
+      version="1.0",
+      description="Folder organizer",
+      url="",
+      author="Yanina Lubenska",
+      author_email="yaninaosadchaya@gmail.com",
+      packages=["clean"]
+      )
